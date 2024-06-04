0188fe1d94bc787e06e3b094c435418f5d2b02cd
yasagy222 <yasaghy222@gmail.com>
2024-06-04 22:53:38 +0330

    obj file

--- obj/Debug/net8.0/OTPService.AssemblyInfo.cs
+++ obj/Debug/net8.0/OTPService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OTPService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b97fd4f652379008a18cdce1250eb53a195dd4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91e7cff9117646e83c9a2da822aedca75d6c57a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("OTPService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OTPService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
